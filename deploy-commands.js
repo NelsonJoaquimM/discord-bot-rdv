a12987ee731551dbb617e811e17b1a8d4054fdbe
NelsonJoaquimM <m.nelsonjoaquim@gmail.com>
2026-04-09 12:51:31 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,31 @@
 console.log("🚀 Script deploy-commands.js lancé !");
-
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
-// Mets ici ton token, ton client ID et ton guild ID
 const TOKEN = "MTQ0ODQxNDQ5Njg3MDYzMzQ4Ng.GM3hV_.mqG9QrEwymhpybcrYVRevqjj6XkEeepU9RftCk";
-const CLIENT_ID = "1448414496870633486"; // Developer Portal → Application → General Information
-const GUILD_ID = "1447883128827019266";   // Clique droit sur ton serveur Discord → Copier l’ID
+const CLIENT_ID = "1448414496870633486";
+const GUILD_ID = "1447883128827019266";
 
 const commands = [
   new SlashCommandBuilder()
     .setName("rdv")
-    .setDescription("Ouvrir le formulaire RDV")
+    .setDescription("Ouvrir le formulaire RDV"),
+
+  new SlashCommandBuilder()
+    .setName("modifier-rdv")
+    .setDescription("Modifier un RDV existant par son ID (statut, date, heure)"),
+
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
   try {
-    console.log("⏳ Enregistrement de la commande /rdv...");
+    console.log("⏳ Enregistrement des commandes...");
     await rest.put(
       Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
       { body: commands }
     );
-    console.log("✅ Commande /rdv enregistrée !");
+    console.log("✅ Commandes /rdv et /modifier-rdv enregistrées !");
   } catch (error) {
     console.error(error);
   }
